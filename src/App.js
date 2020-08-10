d682fe9fe2a82678aba7b0b062f009ee25fc291c
Vitalii Tomchyshak <vitalii.tomchyshak@avid.com>
2020-08-10 13:22:10 +0300

    Testing commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Accordion from './components/Accordion';
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
 
+// Test
+
 const App = () => {
     const items = [
         {
@@ -25,10 +27,10 @@ const App = () => {
             value: 'red',
             content: (
                 <img
-                    alt='red'
-                    src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2Fe%2Fe6%2FRed_rectangle.svg%2F1280px-Red_rectangle.svg.png&f=1&nofb=1'
-                    width='700px'
-                    height='700px'
+                    alt="red"
+                    src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2Fe%2Fe6%2FRed_rectangle.svg%2F1280px-Red_rectangle.svg.png&f=1&nofb=1"
+                    width="700px"
+                    height="700px"
                 />
             ),
         },
@@ -37,10 +39,10 @@ const App = () => {
             value: 'green',
             content: (
                 <img
-                    alt='green'
-                    src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F2%2F25%2FGreen_rectangle.svg%2F2000px-Green_rectangle.svg.png&f=1&nofb=1'
-                    width='700px'
-                    height='700px'
+                    alt="green"
+                    src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F2%2F25%2FGreen_rectangle.svg%2F2000px-Green_rectangle.svg.png&f=1&nofb=1"
+                    width="700px"
+                    height="700px"
                 />
             ),
         },
@@ -49,10 +51,10 @@ const App = () => {
             value: 'blue',
             content: (
                 <img
-                    alt='blue'
-                    src='https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.flagshop.fi%2FWebRoot%2Fvilkasfi01%2FShops%2F2014080403%2F53F0%2FF886%2FBB3A%2F522C%2FCB5B%2F0A28%2F100A%2F2578%2Fblue_rectangle.jpg&f=1&nofb=1'
-                    width='700px'
-                    height='700px'
+                    alt="blue"
+                    src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.flagshop.fi%2FWebRoot%2Fvilkasfi01%2FShops%2F2014080403%2F53F0%2FF886%2FBB3A%2F522C%2FCB5B%2F0A28%2F100A%2F2578%2Fblue_rectangle.jpg&f=1&nofb=1"
+                    width="700px"
+                    height="700px"
                 />
             ),
         },
@@ -62,10 +64,10 @@ const App = () => {
     const [showDropdown, setShowDropdown] = useState(false);
 
     return (
-        <div className='ui container'>
+        <div className="ui container">
             <button
                 onClick={() => setShowDropdown(!showDropdown)}
-                className='button'
+                className="button"
             >
                 Toggle Dropdown
             </button>
