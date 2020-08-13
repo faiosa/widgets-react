de48c11f00634391c3c68925204c0bfe790fabb3
Vitalii Tomchyshak <tomchyshakvitalik@gmail.com>
2020-08-13 22:12:18 +0300

    Enhanced Route logic

--- src/components/Clicker.js
+++ src/components/Clicker.js
@@ -3,21 +3,32 @@ import React, { useState } from 'react';
 export default () => {
     const [count, setCount] = useState(0);
 
-    const onButtonClick = (count) => {
+    const onButtonClick = count => {
         setCount(count + 1);
     };
-    const specialGift = (count) => {
+    const specialGift = count => {
         if (count === 3) {
             return (
-                <img src="https://trinixy.ru/pics5/20170902/3e_sentyabrya_19.jpg" />
+                <img
+                    alt='3'
+                    src='https://trinixy.ru/pics5/20170902/3e_sentyabrya_19.jpg'
+                />
+            );
+        }
+        if (count === 15) {
+            return (
+                <img
+                    alt='15'
+                    src='https://cs11.pikabu.ru/post_img/2019/01/15/4/og_og_1547531060234891506.jpg'
+                />
             );
         }
     };
     return (
-        <div className="ui container">
+        <div className='ui container'>
             <button onClick={() => onButtonClick(count)}>Click Me!</button>
             <h1>Clicked times: {count}</h1>
-            <div className="special">{specialGift(count)}</div>
+            <div className='special'>{specialGift(count)}</div>
         </div>
     );
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,24 @@
 import React from 'react';
+import Link from './Link';
 
 export default () => {
     return (
-        <div className="ui secondary pointing menu">
-            <a href="/" className="item">
+        <div className='ui secondary pointing menu'>
+            <Link href='/' className='item'>
                 Accordion
-            </a>
-            <a href="/search" className="item">
+            </Link>
+            <Link href='/search' className='item'>
                 Search
-            </a>
-            <a href="/dropdown" className="item">
+            </Link>
+            <Link href='/dropdown' className='item'>
                 Dropdown
-            </a>
-            <a href="/translate" className="item">
+            </Link>
+            <Link href='/translate' className='item'>
                 Translator
-            </a>
-            <a href="/clicker" className="item">
+            </Link>
+            <Link href='/clicker' className='item'>
                 Clicker
-            </a>
+            </Link>
         </div>
     );
 };
